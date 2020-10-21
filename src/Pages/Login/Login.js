24cb5c83cdb15d7628715f7a03acafdf309ca342
Vitor <vitor.holler@outlook.pt>
2020-10-21 15:09:57 -0300

    Login com Google

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import{FcGoogle} from 'react-icons/fc'
-import { GoogleLogin } from 'react-google-login';
-import "./Login.css"
+import { FcGoogle } from "react-icons/fc";
+import { GoogleLogin } from "react-google-login";
+import "./Login.css";
+
 const responseGoogle = (response) => {
-    console.log(response);
-  }
+  console.log(response);
+};
 
 const Login = () => {
   return (
@@ -16,13 +17,20 @@ const Login = () => {
           <forms>
             <h1>Entrar</h1>
             <GoogleLogin
-    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-    buttonText="Login"
-    onSuccess={responseGoogle}
-    onFailure={responseGoogle}
-    cookiePolicy={'single_host_origin'}
-  />
-            <button className="googleButton"><FcGoogle value = {{ size: "5em"}}/></button>
+              clientId="225647618283-l615d2dlgp53jar7rop80h75dc22daa3.apps.googleusercontent.com"
+              render={(renderProps) => (
+                <button
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                    <FcGoogle value={{ size: "5em" }} />
+                </button>
+              )}
+              buttonText="Login"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={"single_host_origin"}
+            />
             <h3>Ou insira:</h3>
             <div className="form-group">
               <label for="exampleFormControlInput1">Email</label>
@@ -42,12 +50,13 @@ const Login = () => {
               />
               <a>Esqueceu a senha?</a>
               <h5>Não tem conta?</h5>
-              <a href="https://www.google.com.br" target = "blank">Cadastre-se</a>
+              <a href="https://www.google.com.br" target="blank">
+                Cadastre-se
+              </a>
             </div>
           </forms>
         </div>
       </div>
-    
     </div>
   );
 };
