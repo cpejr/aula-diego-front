8622007062f7fe7698fb560a203b73f3610be442
Daniel Benicá <danielbenica7@gmail.com>
2020-10-30 18:40:12 -0300

    FEAT: transição entre os cards e css
    acho que tem muita coisa errada, porém ta funcionando

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -2,13 +2,28 @@ import React from "react";
 import "./Sidebar.css"
 import Logo from "../../images/logo.png"
 import Foto from "../../images/foto.jpg"
+import {Link,useHistory} from 'react-router-dom'
+
+
 
 const Sidebar = () => {
+
+    const history = useHistory();
+
+    const routeChange = () =>{ 
+      let path = `/`; 
+      history.push(path);
+    }
+
+
     return (
         <div className="sidebarContainer">
             <div className = "logoContainer">
                 <div className="imgContainer">
+                    <Link to='/dashboard'>
                     <img src = {Logo}></img>
+                    </Link>
+                    
                 </div>
             </div>
             
@@ -28,7 +43,7 @@ const Sidebar = () => {
                     <li>Atividades</li>
                 </ul>
                 <div className = "sidebarButtonContainer">
-                    <button className="sidebarButton">SAIR</button>
+                    <button className="sidebarButton" onClick={routeChange}>SAIR</button>
                 </div>
             </div>
         </div>
--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -3,15 +3,20 @@ import Sidebar from "../../Components/Sidebar/Sidebar"
 import Header from "../../Components/Header/Header"
 import Cards from "../../Components/Cards/Cards"
 import "./index.css"
+import Foto from "../../images/samu.svg"
 
 
 const Dashboard = () => {
     return (
-        <div className = 'Teste' style={{display: "flex"}}>
+        <div className = 'Teste' style={{display: "flex",backgroundColor:'#fafafa'}}>
            
             <Sidebar />
             <div style={{flex:1}}>
             <Header /> 
+            <div className='DashboardTitle'>
+            <img src={Foto} className='TitleImg'/>
+            <h1 style={{fontWeight:600}}>Próximas Lives</h1>
+            </div>
              <div className = 'DashboardCardContainer'>
                 <Cards title='SAMU' cardColor = '#A564E5' date = '20/10/2020' hour = '20:00'/>
                 <Cards title='UPA' cardColor = '#6AA5E3' date = '20/10/2020' hour = '20:00' />
