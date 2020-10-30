970c7e8691159ab73fb5f42596315ca6e4c374e2
Vitor <vitor.holler@outlook.pt>
2020-10-30 15:46:58 -0300

    Feat: Página final da live

--- src/Components/TempoLive/tempoLive.js
+++ src/Components/TempoLive/tempoLive.js
@@ -1,11 +1,15 @@
-import React from "react";
-import Sidebar from "../Sidebar/Sidebar";
-import Header from "../Header/Header";
+import React, { useState, useEffect } from 'react';
 import './tempoLive.css'
 
 const TempoLive = (props) => {
+  const [seconds, setSeconds] = useState(0);
 
-
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setSeconds(seconds => seconds + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="tempoLive">
@@ -13,7 +17,7 @@ const TempoLive = (props) => {
         <div className="blocoTempoLive">
           <div className="tituloTempoLive">
             <p>Live Marketing Digital 20/10</p>
-            <p style={{marginTop: "5%" }}>Tempo que você está em Live: </p>
+            <p style={{marginTop: "5%" }}>Tempo que você está em Live: {seconds} segundos</p>
           </div>
           <div className="acessarTempoLive">
             <button className="buttonTempoLive" onClick={props.handleToggle}>Certificar Live</button>
