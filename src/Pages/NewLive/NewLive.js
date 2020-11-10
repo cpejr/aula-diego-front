9c4370324e1f559826e7342d1afbc2ebfda2b59d
Daniel Benicá <danielbenica7@gmail.com>
2020-11-09 21:54:56 -0300

    FEAT: base do formulário pronta

--- src/Pages/NewLive/NewLive.js
+++ src/Pages/NewLive/NewLive.js
@@ -4,6 +4,9 @@ import Header from '../../Components/Header/Header'
 import SideBar from '../../Components/Sidebar/Sidebar'
 
 export default function NewLive() {
+
+
+
     return (
         <div className='NewLiveContainer'>
             <div>
@@ -16,21 +19,26 @@ export default function NewLive() {
             <div className='NewLiveFormBox'>
                <div className='NewLiveInputLine'>
                     <label className='NewLiveLabel'>Título:</label>
-                    <input/>
+                    <input className='NewLiveTitleInput'type='text' placeholder='Digite o título da Live'/>
                </div>
                <div className='NewLiveInputLine'>
                     <label className='NewLiveLabel'>Data:</label>
-                    <input/>
+                    <input className='NewLiveLabelDataInput' type='text'placeholder='DD/MM/AAAA' />
                     <label className='NewLiveLabel'>Horário:</label>
-                    <input/>
+                    <input className='NewLiveLabelTimeInput' type='time'/>
+                    <label className='NewLiveLabel'>Duração:</label>
+                    <input className='NewLiveLabelTimeInput' type='time'/>
                </div>
                <div className='NewLiveInputLine'>
-                    <label className='NewLiveLabel'>Duração:</label>
-                    <input/>
+                    <label className='NewLiveLabel'>Link:</label>
+                    <input className='NewLiveLabelLinkInput'type='text' placeholder='Digite a URL da live'/>
                </div>
                <div className='NewLiveInputLine'>
                     <label className='NewLiveLabel'>Descrição:</label>
-                    <input/>
+                    <textarea placeholder='Digite a descrição da live, com informações e assuntos a serem abordados' className='NewLiveLabelDescriptionInput'/>
+               </div>
+               <div className='NewLiveButtonContainer'>
+                   <button className='NewLiveButton'>Concluir</button>
                </div>
             </div>
                 </div>
