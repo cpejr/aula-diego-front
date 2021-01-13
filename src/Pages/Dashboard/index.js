680034db738f774e1e33862be713dda08f788743
Marcone Faria <marconefaria@ufmg.br>
2021-01-13 17:46:54 -0300

    Ajuste proporcionalidade da tela dashboard

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -5,6 +5,8 @@ import Cards from "../../Components/Cards/Cards";
 import TabelaAtividades from "../../Components/TabelaAtividades/TabelaAtividades";
 import "./index.css";
 import Foto from "../../images/samu.svg";
+import { FaSitemap } from "react-icons/fa";
+import { Directions, MarkunreadMailboxSharp } from "@material-ui/icons";
 
 export default function Dashboard() {
   return (
@@ -13,49 +15,51 @@ export default function Dashboard() {
       style={{
         display: "flex",
         backgroundColor: "#fafafa",
-        overflowX: "hidden",
       }}
     >
-      <Sidebar />
-      <div style={{ flex: 1 }}>
-        <Header />
-        <div className="DashboardTitle">
-          <img src={Foto} className="TitleImg" />
-          <h1 style={{ fontWeight: 600 }}>PRÓXIMAS LIVES</h1>
-        </div>
-        <div style={{ flex: 1, backgroundColor: "#fafafa" }}>
-          <div className="DashboardCardContainer">
-            <Cards
-              title="SAMU"
-              cardColor1="#A564E5"
-              cardColor2="#9244E3"
-              date="20/10/2020"
-              hour="20:00"
-            />
-            <Cards
-              title="UPA"
-              cardColor1="#6AA1E4"
-              cardColor2="#686AE9"
-              date="20/10/2020"
-              hour="20:00"
-            />
-            <Cards
-              title="Bombeiros"
-              cardColor1="#FD88A4"
-              cardColor2="#EE3763"
-              date="20/10/2020"
-              hour="20:00"
+    
+        <Sidebar />
+        <div style={{ flex: 1 }}>
+          <Header />
+          <div className="DashboardTitle">
+            <img src={Foto} className="TitleImg" />
+            <h1 style={{ fontWeight: 600 }}>PRÓXIMAS LIVES</h1>
+          </div>
+          <div style={{ flex: 1, backgroundColor: "#fafafa" }}>
+            <div className="DashboardCardContainer">
+              <Cards
+                title="SAMU"
+                cardColor1="#A564E5"
+                cardColor2="#9244E3"
+                date="20/10/2020"
+                hour="20:00"
+              />
+              <Cards
+                title="UPA"
+                cardColor1="#6AA1E4"
+                cardColor2="#686AE9"
+                date="20/10/2020"
+                hour="20:00"
+              />
+              <Cards
+                title="Bombeiros"
+                cardColor1="#FD88A4"
+                cardColor2="#EE3763"
+                date="20/10/2020"
+                hour="20:00"
+              />
+            </div>
+            <TabelaAtividades
+              type="Lista 1"
+              module="3"
+              name="SAMU"
+              date="22/11/2020"
+              status={true}
             />
           </div>
-          <TabelaAtividades
-            type="Lista 1"
-            module="3"
-            name="SAMU"
-            date="22/11/2020"
-            status={true}
-          />
         </div>
-      </div>
     </div>
   );
 }
+
+
