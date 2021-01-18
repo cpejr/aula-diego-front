c7562a4e0ccf8816a0810a3d5ca5eff21e785cfb
Paulo Carellos <49373945+paulohcarellos@users.noreply.github.com>
2021-01-18 15:00:40 -0300

    Embed video

--- src/Pages/Live/Live.js
+++ src/Pages/Live/Live.js
@@ -31,18 +31,33 @@ const Live = () => {
       <Sidebar />
       <div className="paginaLive">
         <Header />
-      { toggleView && <div className="blocoLive">
-          <div className="tituloLive">
-            <p>Live Marketing Digital 20/10</p>
-            <p style={{fontSize: "x-large"}}>Se inicia às 20h</p>
+        {toggleView && 
+          <div className="blocoLive">
+            <div className="tituloLive">
+              <p>Live Marketing Digital 20/10</p>
+              <p style={{fontSize: "x-large"}}>Se inicia às 20h</p>
+            </div>
+            <div className="acessarLive">
+              <button className="buttonLive" onClick={handleToggle}>ACESSAR</button>
+            </div> 
+          </div>}
+        {!toggleView && toggleView2 &&
+          <div className="blocoLive">
+            <div className="video-Frame">
+              <div className="video-Wrapper">
+                <iframe
+                  src="https://www.youtube.com/embed/1SPy8CBCZJI" 
+                  frameborder="0" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                  allowfullscreen>
+                </iframe>
+              </div>
+            </div>
           </div>
-          <div className="acessarLive">
-            <button className="buttonLive" onClick={handleToggle}>ACESSAR</button>
-          </div> 
-        </div>}
-        {!toggleView && toggleView2 && <TempoLive handleToggle={handleToggle2}/>}
+        }
+        {/* <TempoLive handleToggle={handleToggle2}/> */}
         {!toggleView2 && toggleView3 &&<ConfirmacaoLive handleToggle={handleToggle3}/>}
-       {!toggleView3 && <LiveFinal/>}
+        {!toggleView3 && <LiveFinal/>}
       </div>
     </div>
   );
