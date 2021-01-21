b4d94949719cc219e6362850ac0e70f72b3c126d
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-01-21 10:15:46 -0300

    FIX(PrivateRoute.js): change what renderComponent recieves, add components to useEffect dependencies
    1- rendercomponent recieves a Route instead of the component
    2- when changing between private routes, the url changed but component didnt update. To fix it we needed to inform the component to change the rendercomponent whenever the components from props changed

--- src/Routes/PrivateRoute.js
+++ src/Routes/PrivateRoute.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Component } from "react";
 import { Route, Redirect } from "react-router-dom";
 import { useSession } from "../Context/SessionContext";
 
@@ -8,6 +7,7 @@ export default function PrivateRoute({
   studentComponent,
   adminComponent,
   masterComponent,
+  ...rest
 }) {
   const [loading, setLoading] = useState(true);
   const [renderComponent, setRenderComponent] = useState();
@@ -19,22 +19,29 @@ export default function PrivateRoute({
 
   useEffect(() => {
     const userType = session && session.user.type;
-    console.log(userType);
     switch (userType) {
       case "student":
-        setRenderComponent(studentComponent);
+        setRenderComponent(
+          <Route {...rest} to={path} component={studentComponent} />
+        );
         break;
       case "admin":
-        setRenderComponent(adminComponent);
+        setRenderComponent(
+          <Route {...rest} to={path} component={adminComponent} />
+        );
         break;
       case "master":
-        setRenderComponent(masterComponent);
+        setRenderComponent(
+          <Route {...rest} to={path} component={masterComponent} />
+        );
       default:
-        setRenderComponent(() => <h1>CARREGANDO INFORMAÇÕES</h1>);
-        console.log("default ");
+        setRenderComponent(
+          <Route to={path} component={() => <h1>CARREGANDO</h1>} />
+        );
         break;
     }
-  }, [loading]);
+    console.log(renderComponent);
+  }, [session, studentComponent, adminComponent, masterComponent]);
 
   if (loading) return <h1>CARREGANDO INFORMAÇÕES</h1>;
   if (session && session.accessToken) return renderComponent;
