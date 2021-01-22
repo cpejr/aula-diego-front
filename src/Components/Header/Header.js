764d21c40e74a12218182dbb388c48c928948d3f
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-01-22 11:06:33 -0300

    FIX(Header.js): formatting and updating config page "link"

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import "./Header.css"
-import Foto from "../../images/foto.jpg"
-import {useHistory} from 'react-router-dom'
+import React from "react";
+import "./Header.css";
+import Foto from "../../images/foto.jpg";
+import { useHistory } from "react-router-dom";
 
 const Header = () => {
+  let history = useHistory();
 
-
-    let history = useHistory();
-
-
-  function redirect(path){
-    history.push(path)
+  function redirect(path) {
+    history.push(path);
   }
-    return (
-        <div className="headerContainer">
-            <div className="HeaderElementsContainer">
-                <div className="LabelContainer">
-                    <label className="LabelHeader">Minha Conta</label>
-                    <a className= "aHeader" onClick={()=>redirect('/config-aluno')}>Configurações</a>
-                </div>
-                <img className='HeaderImg'src={Foto}></img>
-            </div>
+  return (
+    <div className="headerContainer">
+      <div className="HeaderElementsContainer">
+        <div className="LabelContainer">
+          <label className="LabelHeader">Minha Conta</label>
+          <a className="aHeader" onClick={() => redirect("/config")}>
+            Configurações
+          </a>
         </div>
-    )
-}
+        <img className="HeaderImg" src={Foto}></img>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
