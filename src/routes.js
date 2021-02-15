a434378e3ed4d498ecf312503dd77bcbc6a8b04b
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-02-15 20:49:10 -0300

    deleted obsolete import

--- src/routes.js
+++ src/routes.js
@@ -13,7 +13,6 @@ import ListaTurma from "./Pages/ListaTurmas/ListaTurmas";
 import NovaTurma from "./Pages/NovaTurma/NovaTurma";
 import Curso from "./Pages/Curso/Curso";
 import NovoCurso from "./Pages/NovoCurso/NovoCurso";
-import ListaCurso from "./Pages/ListaCurso/ListaCurso";
 import ConfigUser from "./Pages/ConfiguracaoUser/ConfiguracaoUser";
 import TabelaTurmas from "./Pages/TabelaTurmas/TabelaTurmas";
 import TurmasAdmin from "./Pages/TurmasAdmin/TurmasAdmin";
