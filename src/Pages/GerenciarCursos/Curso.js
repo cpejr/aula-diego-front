4d5527009093f93b176cc1c2194bb47654d95b1a
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-02-24 07:37:59 -0300

    Fix: fix new course card's link

--- src/Pages/GerenciarCursos/Curso.js
+++ src/Pages/GerenciarCursos/Curso.js
@@ -3,19 +3,27 @@ import "./Curso.css";
 import "./../TurmasAdmin/TurmasAdmin.css";
 import Base from "../../Components/Base/Base";
 import AdmCard from "../../Components/AdmCard/AdmCard";
-import AddIcon from '@material-ui/icons/Add';
-import EditIcon from '@material-ui/icons/Edit';
+import AddIcon from "@material-ui/icons/Add";
+import EditIcon from "@material-ui/icons/Edit";
 
 export default function Curso() {
   return (
     <Base>
       <div className="ContainerTurmasAdmin">
         <div className="TurmasAdmin">
-            <h1 className='ContainerCardsTurmasAdmin' >Criar novo Curso</h1>
-             <div className = 'DashboardCardContainer' >
-              <AdmCard title="Novo Curso" Icon={AddIcon} route="/curso/criar"/>
-              <AdmCard title="Visualizar e Editar" Icon={EditIcon} route="/curso/editar"/>
-            </div> 
+          <h1 className="ContainerCardsTurmasAdmin">Criar novo Curso</h1>
+          <div className="DashboardCardContainer">
+            <AdmCard
+              title="Novo Curso"
+              Icon={AddIcon}
+              route="/cadastro/curso"
+            />
+            <AdmCard
+              title="Visualizar e Editar"
+              Icon={EditIcon}
+              route="/curso/editar"
+            />
+          </div>
         </div>
       </div>
     </Base>
