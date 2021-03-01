e72b87634c32320f833210e285f7f07704c32af2
Vinicius <viniciusflara@gmail.com>
2021-03-01 16:49:02 -0300

    Minor update on admin dashboard

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -30,7 +30,7 @@ export default function Admin() {
             <h1 className='DashBoardTitleFont' >Próximas Lives</h1>
             </div>
              <div className = 'DashboardCardContainer' >
-                <AdmCard title="Adicionar Nova Live" Icon={AddIcon} route="/live" />
+                <AdmCard title="Adicionar Nova Live" Icon={AddIcon} route="/newlive" />
                 <Cards title='UPA' cardColor1 = '#6AA5E3'cardColor2='#686AE9' date = '20/10/2020' hour = '20:00' path={`/cadastro/aula?course=3acce2d3-f52a-4cf0-bdc3-38c2621e46ca`}/>
                 <Cards title='Bombeiros' cardColor1 = '#FD88A4'cardColor2='#EE3763' date = '20/10/2020' hour = '20:00' />
             </div> 
