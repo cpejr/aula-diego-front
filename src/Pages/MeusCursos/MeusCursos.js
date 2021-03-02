d92663f808cb0bb7e89bb78178859b6a305145bb
Marcone Faria <marconefaria@ufmg.br>
2021-03-02 18:35:52 -0300

    Tela listagem de cursos quase linkada com o back

--- src/Pages/MeusCursos/MeusCursos.js
+++ src/Pages/MeusCursos/MeusCursos.js
@@ -1,27 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSession } from "../../Context/SessionContext";
+import { useHistory } from "react-router-dom";
+import api from "../../services/api";
 import Base from "../../Components/Base/Base";
 import { BookOutlined } from '@ant-design/icons';
-import { Upload, message } from 'antd';
+import { message } from 'antd';
 import "./MeusCursos.css";
 
 export default function MeusCursos() {
+    const [courses, setCourses] = useState([]);
+    const [user, setUser] = useState([]);
+    const { session } = useSession();
+    const history = useHistory();
+
+    const config = {
+        headers: {
+        authorization: "BEARER " + session.accessToken,
+        },
+    };
+
+    useEffect(() => {
+        api
+          .get(`/user/${session.user.id}`, config)
+          .then((response) => {
+            setUser(response.data);
+          })
+          .catch(() => {
+            message.error("Não foi possível carregar dados do usuário");
+          });
+        
+        api
+          .get(`/course`, config)
+          .then((response) => {
+            setCourses(response.data);
+          })
+          .catch(() => {
+            message.error("Não foi possível carregar dados dos cursos");
+          });
+        }, 
+      []);
+    
     return (
         <Base>
             <div className="Cursos">
-            <h1 className="TitleCursos"><BookOutlined />Meus Cursos</h1>
-                <div className="containerCursos">
-                    <div className="CardCursoRight">
-                        <h1 className="TitleCurso">Programação Orientada a Objetos</h1>
-                        <div className="videoAula">
-                            
-                        </div>  
-                    </div>
-                    <div className="CardCursoLeft">
-                        <h4 className="descriptionCurso">Programação orientada a objetos é um paradigma de programação baseado no conceito de "objetos", que podem conter dados na forma de campos, também conhecidos como atributos, e códigos, na forma de procedimentos, também conhecidos como métodos.</h4>
-                        <div className="videoAula">
-                            
-                        </div>  
-                    </div>
+                <div className="description">
+                    <h1 className="TitleCursos"><BookOutlined />Meus Cursos</h1>
+                    {courses
+                        ? courses.map((course) => {
+                        return (
+                            <div className="containerCursos">
+                                <div className="CardCursoTop">
+                                    <h4 className="descriptionCurso"></h4> 
+                                </div>
+                                <div className="CardCursoBottom">
+                                    <h3 className="TitleCurso">{course.name}</h3>
+                                    <h6 className="subTitleCurso">{course.organization_id}</h6>
+                                    <h6 className="subTitleCurso">{course.description}</h6>
+                                    <button className="btnVerCurso" onClick={() => history.push(`/curso/${course.id}`)}>
+                                        Ver Curso
+                                    </button>
+                                </div>
+                            </div>
+                        );
+                        })
+                    : null}
                 </div>
             </div>
         </Base>
