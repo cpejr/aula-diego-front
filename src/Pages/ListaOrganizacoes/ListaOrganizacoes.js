e068f52ce80367decdfeef3025f77d5d61d93ae3
Marcone Faria <marconefaria@ufmg.br>
2021-03-02 20:53:32 -0300

    pequenos erros

--- src/Pages/ListaOrganizacoes/ListaOrganizacoes.js
+++ src/Pages/ListaOrganizacoes/ListaOrganizacoes.js
@@ -3,7 +3,7 @@ import { useSession } from "../../Context/SessionContext";
 import { useHistory } from "react-router-dom";
 import api from "../../services/api";
 
-import { Table, Tag, Input, Tooltip, message, Popconfirm } from "antd";
+import { Table, Tag, Input, Tooltip, message, Popconfirm, Modal } from "antd";
 import DeleteIcon from "@material-ui/icons/DeleteForever";
 import EditIcon from "@material-ui/icons/Edit";
 import AddIcon from '@material-ui/icons/Add';
@@ -14,8 +14,12 @@ import "./ListaOrganizacoes.css";
 
 export default function ListaOrganizacoes() {
   const [organization, setOrganizations] = useState([]);
+  const [isModalEditVisible, setIsModalEditVisible] = useState(false);
+  const [editOrganizationId, setEditOrganizationId] = useState();
+  const [formData, setformData] = useState([]);
   const [data, setData] = useState("");
   const [search, setSearch] = useState(data);
+  const [loading, setLoading] = useState(true);
   const { session } = useSession();
   const history = useHistory();
 
@@ -30,6 +34,7 @@ export default function ListaOrganizacoes() {
       .get(`/organization`, config)
       .then((response) => {
         setOrganizations(response.data);
+        setLoading(false);
       })
       .catch(() => {
         message.error("Não foi possível carregar dados das organizações");
@@ -78,7 +83,7 @@ export default function ListaOrganizacoes() {
       dataIndex: ("id"),
       render: (id) => (
         <>
-          <EditIcon className="clickable" onClick={handleEdit} />{" "}
+          <EditIcon className="clickable" onClick={() => openEditModal(id)} />{" "}
           <Popconfirm
             title="Tem certeza que deseja excluir este item?"
             onConfirm={() => handleDelete(id)}
@@ -90,7 +95,12 @@ export default function ListaOrganizacoes() {
     },
   ];
 
+  function handleFormChange(e) {
+    setformData({ ...formData, [e.target.name]: e.target.value });
+  }
+
   function handleDelete(organization_id) {
+    setLoading(true);
     api
       .put(`/organization/${organization_id}`, {}, config)
       .then(() => message.success("Deletado com sucesso"))
@@ -99,6 +109,7 @@ export default function ListaOrganizacoes() {
           .then((response) => { 
             setOrganizations(response.data); 
           })
+          .then(setLoading(false));
       })
       .catch((error) => {
         message.error("Não foi possível excluir");
@@ -107,8 +118,33 @@ export default function ListaOrganizacoes() {
       );
   }
 
-  function handleEdit() {
-    alert("EDIT ainda não faz nada. tururu");
+  function openEditModal(id) {
+    setEditOrganizationId(id);
+    setIsModalEditVisible(true);
+  }
+
+  function handleOk() {
+    const wantsToEdit = window.confirm(
+      "Você tem certeza que deseja alterar essa organização?"
+    );
+    if (!wantsToEdit) return message.error("Operação cancelada");
+    else
+      api
+        .post(`/organization/${editOrganizationId}`, formData, config)
+        .then(() => message.success("Organização alterada com sucesso"))
+        .then(() => {
+          api.get("/organization", config)
+            .then((response) => { 
+              setOrganizations(response.data); 
+            })
+            .then(setLoading(false));
+        })
+        .catch(() => message.error("Não foi possível editar a organização"));
+  }
+
+  function handleCancel() {
+    setIsModalEditVisible(false);
+    return message.error("Operação cancelada");
   }
 
   function handleChange(value) {
@@ -133,8 +169,28 @@ export default function ListaOrganizacoes() {
         <Table
           columns={columns}
           dataSource={organization}
+          loading={loading}
         />
       </div>
+      <Modal
+        title="Editar usuário"
+        visible={isModalEditVisible}
+        onOk={() => handleOk()}
+        onCancel={() => handleCancel()}
+      >
+        <Input
+          name="name"
+          value={formData["name"]}
+          onChange={handleFormChange}
+          placeholder="Nome"
+        />
+        <Input
+          name="description"
+          value={formData["description"]}
+          onChange={handleFormChange}
+          placeholder="Descrição"
+        />
+      </Modal>
     </Base>
   );
 }
