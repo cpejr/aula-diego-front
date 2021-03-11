0e53e60c9c05884f8edc9210d155f58d972ae339
Marcone Faria <marconefaria@ufmg.br>
2021-03-11 15:51:40 -0300

    Link da página live

--- src/Pages/Live/Live.js
+++ src/Pages/Live/Live.js
@@ -1,26 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useSession } from "../../Context/SessionContext";
 import Base from "../../Components/Base/Base";
-import Header from "../../Components/Header/Header";
+import api from "../../services/api";
+import { message } from "antd";
 import TempoLive from "../../Components/TempoLive/TempoLive.js";
 import ConfirmacaoLive from "../../Components/ConfirmacaoLive/ConfirmacaoLive"
 import LiveFinal from '../../Components/LiveFinal/LiveFinal'
 import VideoFrame from '../../Components/VideoFrame/VideoFrame'
 import "./Live.css";
 
-const Live = () => {
-
-  let url = '2-XVG6Vg8_o'
-
-  const [toggleViewInfo, setToggleViewInfo] = useState(true)
-  const [toggleViewVideo, setToggleViewVideo] = useState(false)
-  const [toggleView3, setToggleView3] = useState(false)
-
+const Live = (props) => {
+  const [toggleViewInfo, setToggleViewInfo] = useState(true);
+  const [toggleViewVideo, setToggleViewVideo] = useState(false);
+  const [toggleView3, setToggleView3] = useState(false);
 
   function handleToggle() {
-    setToggleViewInfo(false)
-    setToggleViewVideo(true)
+    setToggleViewInfo(false);
+    setToggleViewVideo(true);
   }
 
+  const { session } = useSession();
+  const [liveData, setLiveData] = useState();
+  const { id } = props.match.params;
+  
+  const config = {
+    headers: {
+      authorization: "BEARER " + session.accessToken,
+    },
+  };
+
+  useEffect(() => {
+    api
+      .get(`/live/${id}`, config)
+      .then((response) => {
+        setLiveData(response.data);
+      })
+      .catch(() => {
+        message.error("Não foi possível carregar dados da live");
+      });
+  }, []);
+
+  function dateFormate(){	
+    var data = new Date(liveData && liveData.date);
+    return data.toLocaleDateString([], {dateStyle: 'short'}) + ' ' + data.toLocaleTimeString([], {timeStyle: 'short'});
+  }
+
+  function formatLink(){
+    let url = liveData && liveData.link;
+    var shortlink = url.split("=");
+
+    return shortlink[1];
+  }
 
   return (
     <Base>
@@ -28,13 +59,14 @@ const Live = () => {
         <div className="paginaLive">
           <div className="blocoLive">
             <div className="tituloLive">
-              <p>Live Marketing Digital 20/10</p>
+              <p>{liveData && liveData.name}, {dateFormate()}</p>
             </div>
             {toggleViewInfo && <div className="acessarLive">
               <button className="buttonAccessLive" onClick={handleToggle}>ACESSAR</button>
             </div>}
             {toggleViewVideo && <div className="videoFrame">
-              <VideoFrame url={'https://www.youtube.com/embed/' + String(url)} />
+              <VideoFrame url={'https://www.youtube.com/embed/' + String(formatLink())} />
+              <h5 className="liveDescription">{liveData && liveData.description}</h5>
             </div>}
           </div>
           {toggleViewVideo && <div className="certificateWrapper">
