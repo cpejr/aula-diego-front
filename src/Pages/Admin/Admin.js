f410e143c0d1d37d4b947d6a34bd51fe028ee743
Paulo Henrique Machado Carellos <49373945+paulohcarellos@users.noreply.github.com>
2021-04-28 16:52:18 -0300

    almost

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Base from "../../Components/Base/Base";
 import api from "../../services/api";
-import { message, Timeline, Divider } from "antd";
+import { message, Timeline, Divider, Card, Statistic } from "antd";
+import { UserOutlined } from "@ant-design/icons"
 import { useSession } from "../../Context/SessionContext";
 import { useHistory } from "react-router-dom";
 import "./Admin.css";
@@ -10,6 +11,8 @@ import "./Admin.css";
 export default function Admin() {
 
   const [students, setStudents] = useState([]);
+  const [approved, setApproved] = useState([]);
+  const [pending, setPending] = useState([]);
   const [courses, setCourses] = useState([]);
   const [past, setPast] = useState(false);
   const [future, setFuture] = useState(false);
@@ -33,8 +36,20 @@ export default function Admin() {
   useEffect(() => {
     api
       .get(`/user`, { ...config, params: { "user.organization_id": session.user.organization_id } })
-      .then(response => setStudents(response.data))
-      .catch(err => { message.error("Não foi possível carregar dados das aulas"); })
+      .then(response => {
+        let app = 0;
+        let pen = 0;
+
+        response.data.map(student => {
+          if (student.status === 'approved') app += 1;
+          if (student.status === 'pending') pen += 1;
+        });
+
+        setStudents(response.data);
+        setApproved(app);
+        setPending(pen);
+      })
+      .catch(err => { message.error("Não foi possível carregar dados dos estudantes"); })
 
     const list = []
 
@@ -98,21 +113,50 @@ export default function Admin() {
   return (
     <Base>
       <div className='adminRoot'>
-        <h4>Últimas atualizações</h4>
-        <div className="adminTimelineFuture">
-          <Timeline mode={'left'} reverse={true}>
-            {future && future.map(item => (
-              <Timeline.Item label={`${item.date} - ${item.course_name}`} color={item.color}>{item.name}</Timeline.Item>
-            ))}
-          </Timeline>
+        <div className="adminStatisticsWrapper">
+          <Card bordered={false} style={{ width: "20%" }}>
+            <Statistic
+              title="Estudantes"
+              value={approved}
+              prefix={<UserOutlined />}
+            />
+          </Card>
+          <Card bordered={false} style={{ width: "20%" }}>
+            <Statistic
+              title="Pendentes"
+              value={pending}
+            />
+          </Card>
+          <Card bordered={false} style={{ width: "20%" }}>
+            <Statistic
+              title="Cursos"
+              value={courses.length}
+            />
+          </Card>
+          <Card bordered={false} style={{ width: "20%" }}>
+            <Statistic
+              title="Pendentes"
+              value={pending.length}
+            />
+          </Card>
         </div>
-        <Divider />
-        <div className="adminTimelinePast">
-          <Timeline mode={'right'} reverse={true}>
-            {past && past.map(item => (
-              <Timeline.Item label={`${item.course_name} - ${item.date}`} color={item.color}>{item.name}</Timeline.Item>
-            ))}
-          </Timeline>
+        <h3>Linha do Tempo</h3>
+        <div className="adminTimelinesWrapper" >
+          <div className="adminTimelineFuture">
+            <Timeline mode={'left'} reverse={true}>
+              {future && future.map(item => (
+                <Timeline.Item label={`${item.date} - ${item.course_name}`} color={item.color}>{item.name}</Timeline.Item>
+              ))}
+            </Timeline>
+          </div>
+          <Divider style={{ marginBottom: "36px", marginTop: "0px" }} />
+          <div className="adminTimelinePast">
+            <Timeline mode={'right'} reverse={true}>
+              {past && past.map(item => (
+                <Timeline.Item label={`${item.course_name} - ${item.date}`} color={item.color}>{item.name}</Timeline.Item>
+              ))}
+            </Timeline>
+          </div>
         </div>
       </div>
     </Base>
