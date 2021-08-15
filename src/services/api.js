a00ce2463898caf258ab5b9158ee5e2a3a29bf6a
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-08-14 22:43:00 -0300

    update backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:6969",
-  // baseURL: "https://aula-diego.herokuapp.com",
+  baseURL: "https://auladiegoback.herokuapp.com",
 });
 
 export default api;
