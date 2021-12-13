6f1fb8328a060a5ce41752ccfd6766021ff0f5b4
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-12-13 08:50:09 -0300

    feat: create handleError helper

---
+++ src/utils/handleError.js
@@ -0,0 +1,26 @@
+import { message } from "antd";
+
+const defaultErrors = {
+  400: "Erro de validação",
+  401: "Não autorizado",
+  404: "Não encontrado",
+  403: "Acesso negado",
+  408: "Tempo limite excedido",
+  409: "Conflito",
+  422: "Erro de validação",
+  429: "Limite de requisições excedido",
+  500: "Erro interno do servidor",
+  502: "Erro de comunicação",
+  503: "Serviço indisponível",
+  504: "Tempo limite excedido",
+};
+
+export default function handleError(error, message) {
+  const { response } = error;
+  if (response && response.status > 299) {
+    const { status } = response;
+    const messageError =
+      response.data.message || message || defaultErrors[status];
+    message.error(messageError);
+  }
+}
