845be4ba4c3a14d6883df68d6761e8ad03c39c82
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2022-01-26 22:33:51 -0300

    generate build

---
+++ build/precache-manifest.bc4cab8330aca4f34408522be4eaa989.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e54d01a8931939a81c34183be81f4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c7521e2b01c45c85eb8",
+    "url": "/static/css/2.dd2ca793.chunk.css"
+  },
+  {
+    "revision": "153fb75e8ee455af8122",
+    "url": "/static/css/main.347451e6.chunk.css"
+  },
+  {
+    "revision": "4c7521e2b01c45c85eb8",
+    "url": "/static/js/2.99822208.chunk.js"
+  },
+  {
+    "revision": "9901e7c3e6ada12bd25cdfbc4078f150",
+    "url": "/static/js/2.99822208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153fb75e8ee455af8122",
+    "url": "/static/js/main.832e1311.chunk.js"
+  },
+  {
+    "revision": "474c51e96fe911290353",
+    "url": "/static/js/runtime-main.3a1be024.js"
+  },
+  {
+    "revision": "442d03e10526c6a414ddea5139fe5a73",
+    "url": "/static/media/Logo2.442d03e1.png"
+  },
+  {
+    "revision": "7f39078d3021b5d926decf272a251116",
+    "url": "/static/media/data.7f39078d.svg"
+  },
+  {
+    "revision": "ee03af0e002ffb4a89e54dbadcc4a725",
+    "url": "/static/media/lesson.ee03af0e.svg"
+  },
+  {
+    "revision": "1905e79f3c25707e4b48b4de3f9ced40",
+    "url": "/static/media/logo-samu-home.1905e79f.png"
+  },
+  {
+    "revision": "3a3c84cc42e843c9ba45583b854b2c63",
+    "url": "/static/media/logo-upa-home.3a3c84cc.svg"
+  },
+  {
+    "revision": "1c8f5ccaf2aefc739b89e0a211eb4e78",
+    "url": "/static/media/logo.1c8f5cca.png"
+  },
+  {
+    "revision": "f9177a620bb7b2ba1ee1b7e4fc9a586d",
+    "url": "/static/media/reclas.f9177a62.svg"
+  },
+  {
+    "revision": "5b8240ae7188a46a0bb7bf91e28c92e1",
+    "url": "/static/media/reclass-aluno.5b8240ae.svg"
+  },
+  {
+    "revision": "ec5359597ab27d9f7b29b710ee545341",
+    "url": "/static/media/rocket.ec535959.svg"
+  }
+]);
