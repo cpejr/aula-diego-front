4e7528c138a5f8b98cb6554a0e736fb934209ac6
Gustavo Gomes <gustavo.sgomes@dtidigital.com.br>
2022-01-26 21:48:25 -0300

    remove build folder from gitignore

---
+++ build/precache-manifest.5e1d1d84751b69ba16abc2637be7e453.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121cb5ffe93c7c4d130d888f36a55674",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca9ac1ecc2ea38405185",
+    "url": "/static/css/2.67ef2cbb.chunk.css"
+  },
+  {
+    "revision": "cb1b60b3ce76c253ce3a",
+    "url": "/static/css/main.3016bf9c.chunk.css"
+  },
+  {
+    "revision": "ca9ac1ecc2ea38405185",
+    "url": "/static/js/2.183d18b1.chunk.js"
+  },
+  {
+    "revision": "082c1bdf24861af2babbd4591bc978fa",
+    "url": "/static/js/2.183d18b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb1b60b3ce76c253ce3a",
+    "url": "/static/js/main.60ce61f1.chunk.js"
+  },
+  {
+    "revision": "474c51e96fe911290353",
+    "url": "/static/js/runtime-main.3a1be024.js"
+  },
+  {
+    "revision": "442d03e10526c6a414ddea5139fe5a73",
+    "url": "/static/media/Logo2.442d03e1.png"
+  },
+  {
+    "revision": "7f39078d3021b5d926decf272a251116",
+    "url": "/static/media/data.7f39078d.svg"
+  },
+  {
+    "revision": "ee03af0e002ffb4a89e54dbadcc4a725",
+    "url": "/static/media/lesson.ee03af0e.svg"
+  },
+  {
+    "revision": "1905e79f3c25707e4b48b4de3f9ced40",
+    "url": "/static/media/logo-samu-home.1905e79f.png"
+  },
+  {
+    "revision": "3a3c84cc42e843c9ba45583b854b2c63",
+    "url": "/static/media/logo-upa-home.3a3c84cc.svg"
+  },
+  {
+    "revision": "1c8f5ccaf2aefc739b89e0a211eb4e78",
+    "url": "/static/media/logo.1c8f5cca.png"
+  },
+  {
+    "revision": "f9177a620bb7b2ba1ee1b7e4fc9a586d",
+    "url": "/static/media/reclas.f9177a62.svg"
+  },
+  {
+    "revision": "5b8240ae7188a46a0bb7bf91e28c92e1",
+    "url": "/static/media/reclass-aluno.5b8240ae.svg"
+  },
+  {
+    "revision": "ec5359597ab27d9f7b29b710ee545341",
+    "url": "/static/media/rocket.ec535959.svg"
+  }
+]);
