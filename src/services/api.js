8521b421f79606f321a2721eeb42def54d96c90e
Gustavo Gomes <gustavo.sgomes@dtidigital.com.br>
2022-01-26 21:20:03 -0300

    muda rota backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:6969",
-  // baseURL: "https://aula-diego.herokuapp.com",
+  baseURL: "http://localhost:3001",
 });
 
 export default api;
